e01c5c8ebf34831b8677a69609ff6e08931448c5
Jonathan Steinert <hachi@kuiki.net>
2015-04-08 22:52:08 -0700

    Much more advanced

--- aquarium_clock.ino
+++ aquarium_clock.ino
@@ -1,3 +1,4 @@
+#include "U8glib.h"
 #include <Time.h>
 #include <Timezone.h>
 
@@ -5,39 +6,26 @@
 //#define BATHROOM
 
 #ifdef BEDROOM
-#define HAS_LCD
-#include <LiquidCrystal.h>
 #include <Process.h>
-
-#define BLUE_PIN  3
-#define WHITE_PIN 11
 
 #define TIME_PROVIDER get_time
 #endif
 
 #ifdef BATHROOM
-#define HAS_LCD
-#include <LiquidCrystal_I2C.h>
 #include <Wire.h>         //http://arduino.cc/en/Reference/Wire (included with Arduino IDE)
 #include <DS3232RTC.h>    //http://github.com/JChristensen/DS3232RTC
+
+#define TIME_PROVIDER RTC.get
+#endif
 
 #define BLUE_PIN  10
 #define WHITE_PIN 11
 #define BACK_PIN 9
 
-#define TIME_PROVIDER RTC.get
-#endif
-
 #define PI 3.141592653589
 #define RAD_PER_DAY (24 / (2 * PI))
 
-#ifdef BEDROOM
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // 10 is the bloody backlight
-#endif
-
-#ifdef BATHROOM
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-#endif
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
 
 TimeChangeRule myDST = {"PDT", Second, Sun, Mar, 2, -420};    //Daylight time = UTC - 7 hours
 TimeChangeRule mySTD = {"PST",  First, Sun, Nov, 2, -480};    //Standard time = UTC - 8 hours
@@ -63,114 +51,104 @@ time_t get_time() {
   while (date.running()) {
     delay(50);
   }
- 
+
   if (date.available() > 0) {
     String timeString = date.readString();
     date.flush();
     return timeString.toInt();
   }
-  
+
   return 0;
 }
 #endif
 
 void setup() {
+  u8g.begin();
 #ifdef BEDROOM
   Bridge.begin();        // initialize Bridge
-  lcd.begin(16, 2);
 #endif
-#ifdef BATHROOM
-  lcd.init();
-  lcd.backlight();
-#endif
-  lcd.setCursor(0, 0);
-  lcd.print(F("Clock syncing"));
 
   setSyncProvider(TIME_PROVIDER);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  if (timeStatus() != timeSet) {
-    lcd.print(F("Clock sync fail"));
-  }
-  else {
-    lcd.print(F("Clock synced"));
-  }
-  
-  delay(2000);
-  lcd.clear();
-
-#ifdef BACK_PIN  
-  analogWrite(BACK_PIN, 50);
-#endif
 }
 
+int prevBlue = -1;
+int prevWhite = -1;
+
 void loop() {
-  static int prevBlue = -1;
-  static int prevWhite = -1;
-  
   utc = now();
   local = myTZ.toLocal(utc, &tcr);
 
-  digitalClockDisplay(local);
-
   float x = hour(local) + ((minute(local) + (second(local) / 60.0)) / 60.0);
   float curve = sin ((x - 9.0) / RAD_PER_DAY);
-  
+
   int white = (curve +  .5 ) / 1.5  * 255;
   int blue  = (curve + 1.25) / 2.25 * 255;
-  
+
   if (white < 0)
     white = 0;
-    
+
   if (blue < 0)
     blue = 0;
-    
+
   if (prevWhite != white) {
-    lcd.setCursor(0, 1);
-    lcd.print(F("W:"));
-    printDigits(white, ' ', 3);
     analogWrite(WHITE_PIN, white);
+    analogWrite(BACK_PIN, white);
     prevWhite = white;
   }
-  
+
   if (prevBlue != blue) {
-    lcd.setCursor(10, 1);
-    lcd.print(F("B:"));
-    printDigits(blue, ' ', 3);
     analogWrite(BLUE_PIN, blue);
     prevBlue = blue;
   }
-  
+
+  render();
   delay(50);
+}
+
+void render()
+{
+  u8g.firstPage();
+  do {
+    draw();
+  } while ( u8g.nextPage() );
+}
+
+void draw()
+{
+//  u8g.setFont(u8g_font_unifont);
+  u8g.setFont(u8g_font_4x6);
+  u8g.setPrintPos(1, 30);
+  u8g.print(F("Blue: "));
+  u8g.print(prevBlue);
+
+  u8g.setPrintPos(1, 45);
+  u8g.print(F("White: "));
+  u8g.print(prevWhite);
+
+  digitalClockDisplay(local);
 }
 
 void digitalClockDisplay(time_t t)
 {
-  static int prevsec = 62;
-  int seconds = second(t);
-  if (seconds == prevsec)
-    return;
-  prevsec = seconds;
-  lcd.setCursor(0, 0);
+  u8g.setPrintPos(1, 15);
   printDigits(hour(t));
-  lcd.print(F(":"));
+  u8g.print(':');
   printDigits(minute(t));
-  lcd.print(F(":"));
-  printDigits(seconds);
+  u8g.print(':');
+  printDigits(second(t));
 }
 
 void printDigits(int number, char leader, unsigned int lead)
 {
-  int digits = log(number)/log(10);
+  int digits = log(number) / log(10);
   for (int i = digits + 1; i < lead; i++) {
-    lcd.print(leader);
+    u8g.print(leader);
   }
-    
-  lcd.print(number);
+
+  u8g.print(number);
 }
 
 void printDigits(int digits)
 {
   printDigits(digits, '0', 2);
 }
-
